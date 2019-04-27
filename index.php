530d4b06321f1dfe307e3559f53d8b32fd2a7a8a
Danno980117 <Daniel.E.A.Ramirez@gmail.com>
2019-04-26 19:30:55 -0500

    agregamos
    utilizamos style modificamos la expresion en if.
    agregamos nombres y datos

--- index.php
+++ index.php
@@ -1,15 +1,22 @@
 <?php
 	$cantidad=0;
-	$nombres;
+	$nombres='';
 	//PHP es una lenguaje de expresiones
-	?>
-	<table>
+?>
+<table>
 		<tr>
 			<th>Nombres</th>
 			<th>Apellidos</th>
 			<th>DNI</th>
 		</tr>
-<?php if ($nombres): ?>
+<?php if (1): ?>
+	<style type="text/css">
+		td{
+			font-family: Arial;
+			background: green;
+			color: white;
+		}
+	</style>
 	<tr>
 		<td>Luis</td>
 		<td>Gutierrez</td>
